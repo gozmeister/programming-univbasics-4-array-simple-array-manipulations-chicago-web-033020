49fd36e6c8614271a068340af57c111d0524af82
Sam Gorick <samgorick.sg@gmail.com>
2020-03-07 17:47:02 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push (array, string)
+  array.push(string)
+end
+
+def using_unshift (array, string)
+  array.unshift(string)
+end
+
+def using_pop (array)
+  array.pop 
+end
+
+def pop_with_args (array)
+  array.pop(2)
+end
+
+def using_shift (array)
+  array.shift 
+end
+
+def shift_with_args (array)
+  array.shift(2)
+end
+
+def using_concat(arg1, arg2)
+  arg1.concat(arg2)
+end
+
+def using_insert (array, argument)
+  array.insert(4, argument)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
